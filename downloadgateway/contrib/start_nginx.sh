82fbed5c5d2ce73ac6e2163a3bd66e49d2c4f896
17Halbe <github-alex@gar-nich.net>
2017-12-15 10:42:24 +0100

    Activated Let's Encrypt

--- downloadgateway/contrib/start_nginx.sh
+++ downloadgateway/contrib/start_nginx.sh
@@ -2,7 +2,7 @@
 # Note: I've written this using sh so it works in the busybox container too
 echo "installing /etc/nginx/conf.d/download.conf:"
 
-/bin/sed 's|<servername_replaced_during_buildtime>|'"$NGINX_SERVERNAMES"'|g' /tmp/conf.d/download.conf > /etc/nginx/conf.d/download.conf
+/bin/sed 's|<servername_replaced_during_buildtime>|downloads.'"$DOMAINNAME}"'|g' /tmp/conf.d/download.conf > /etc/nginx/conf.d/download.conf
 /bin/sed 's|<shared_secret_replaced_during_buildtime>|'"$NGINX_SHARED_SECRET"'|g' /etc/nginx/conf.d/download.conf
 #/bin/cat /etc/nginx/conf.d/download.conf
 # start service in background: 
