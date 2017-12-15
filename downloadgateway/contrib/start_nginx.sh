e0fa70a7fb5ccfb0b3bb641db11508bc91534102
17Halbe <github-alex@gar-nich.net>
2017-12-15 10:08:03 +0100

    made nginx config non permanent

--- downloadgateway/contrib/start_nginx.sh
+++ downloadgateway/contrib/start_nginx.sh
@@ -2,8 +2,8 @@
 # Note: I've written this using sh so it works in the busybox container too
 echo "installing /etc/nginx/conf.d/download.conf:"
 
-/bin/sed -i 's|<servername_replaced_during_buildtime>|'"$NGINX_SERVERNAMES"'|g' /etc/nginx/conf.d/download.conf
-/bin/sed -i 's|<shared_secret_replaced_during_buildtime>|'"$NGINX_SHARED_SECRET"'|g' /etc/nginx/conf.d/download.conf
+/bin/sed 's|<servername_replaced_during_buildtime>|'"$NGINX_SERVERNAMES"'|g' /tmp/conf.d/download.conf > /etc/nginx/conf.d/download.conf
+/bin/sed 's|<shared_secret_replaced_during_buildtime>|'"$NGINX_SHARED_SECRET"'|g' /etc/nginx/conf.d/download.conf
 #/bin/cat /etc/nginx/conf.d/download.conf
 # start service in background: 
 nginx -g 'daemon off;' 
