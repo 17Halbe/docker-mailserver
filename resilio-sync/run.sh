5ae599fa3bf77830d61f4e1d388ec12f765aac7d
17Halbe <github-alex@gar-nich.net>
2017-12-31 01:28:23 +0100

    Made Resilio work and less verbose

--- resilio-sync/run.sh
+++ resilio-sync/run.sh
@@ -14,7 +14,8 @@ for dir in $( grep "dir" /tmp/resilio-config/conf.d/*.conf | cut -d'"' -s -f4 )
 do
 	case "$dir" in 
 	/*) mkdir -p $dir ;;
-	*) mkdir -p /usr/share/resilio-sync/$dir ;;
+	*) mkdir -p /usr/share/resilio-sync/$dir 
+	   sed -i "s|$dir|/usr/share/resilio-sync/$dir|" /tmp/resilio-config/sync.conf;;
 	esac
 	
 done
