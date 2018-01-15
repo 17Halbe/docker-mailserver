9635656cddf1e64e1847e448e081f94c3df1f2f2
17Halbe <github-alex@gar-nich.net>
2018-01-15 09:32:14 +0100

    implemented downloadcontainer

--- downloadgateway/contrib/start_nginx.sh
+++ downloadgateway/contrib/start_nginx.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# Note: I've written this using sh so it works in the busybox container too
 echo "installing /etc/nginx/conf.d/download.conf:"
 
-/bin/sed 's|<servername_replaced_during_buildtime>|downloads.'"$DOMAINNAME}"'|g' /tmp/conf.d/download.conf > /etc/nginx/conf.d/download.conf
-/bin/sed 's|<shared_secret_replaced_during_buildtime>|'"$NGINX_SHARED_SECRET"'|g' /etc/nginx/conf.d/download.conf
+/bin/sed 's|<servername_replaced_during_startup>|'"$VIRTUAL_HOST"'|g' /tmp/conf.d/download.conf > /etc/nginx/conf.d/download.conf
+/bin/sed -i 's|<shared_secret_replaced_during_startup>|'"$NGINX_SHARED_SECRET"'|g' /etc/nginx/conf.d/download.conf
 #/bin/cat /etc/nginx/conf.d/download.conf
 # start service in background: 
 nginx -g 'daemon off;' 
